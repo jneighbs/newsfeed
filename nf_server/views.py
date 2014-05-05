79ae7e82a307a6e2ea6e63f72eada1f8045bd28d
thesamfest <thesam@stanford.edu>
2014-05-04 21:18:48 -0700

    Updated NewsEvent model
    derping around with Event creation front end.

--- nf_server/views.py
+++ nf_server/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from tasks import slowAdd, classify, trainClassifier
+from nf_server.models import NewsEvent, Article, NewsEventForm
 
 # Create your views here.
 def index(request):
@@ -10,8 +11,19 @@ def index(request):
 def event(request, event_id):
 	return HttpResponse("event %s - newsfeed.com/event" % event_id)
 
-def createEvent(request):
-	return render(request, 'create_event.html', {})
+def createEvent(request, event_id=None):
+
+	if event_id:
+		print "got an id"
+		event = get_object_or_404(NewsEvent, pk=event_id)
+		form = NewsEventForm(instance=event)
+
+	else:
+		print "ain't got no event id"
+		form = NewsEventForm()
+	
+
+	return render(request, 'create_event.html', {'form': form})
 	#return HttpResponse("So you wanna create an event, eh?")
 
 def newEvent(request):
