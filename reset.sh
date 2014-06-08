a26e065a24dde6949f8c7c114d28cf8ec4c5baaf
Marie Feng <marie.feng@gmail.com>
2014-06-08 16:17:40 -0700

    New fake data

--- reset.sh
+++ reset.sh
@@ -2,4 +2,4 @@
 python manage.py sqlclear nf_server | sqlite3 db.sqlite3
 python manage.py sql nf_server | sqlite3 db.sqlite3
 # python manage.py loaddata dummy2.yaml
-python manage.py loaddata dummy3.yaml
+python manage.py loaddata dummy4.yaml
